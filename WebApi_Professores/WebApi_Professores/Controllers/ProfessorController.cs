619477b1dbdb295549c530a03bde37979cd402ea
AndradeMiguel95 <mig_andrad@hotmail.com>
2021-05-25 16:50:47 -0400

    Alteração Iniciail

---
+++ WebApi_Professores/WebApi_Professores/Controllers/ProfessorController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi_Professores.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi_Professores.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+    public class ProfessorController : ControllerBase
+    {
+        // GET: api/Professor
+        public IEnumerable<Professores> Get()
+        {
+            Professores professor = new Professores();
+            return professor.ListaProfessores();
+        }
+
+        // GET: api/Professor/5
+        public Professores Get(int id)
+        {
+            Professores professor = new Professores();
+            return professor.ListaProfessores().Where(x => x.Mid == id).FirstOrDefault();
+        }
+
+        // POST: api/Professor
+        public List<Professores> Post(Professores professor)
+        {
+            Professores _professor = new Professores();
+            _professor.Inserir(professor);
+            return _professor.ListaProfessores();
+        }
+
+        // PUT: api/Professor/5
+        public List<Professores> Put(int id, Professores professor)
+        {
+            Professores _professor = new Professores();
+            _professor.Atualizar(id, professor);
+            return _professor.ListaProfessores();
+
+        }
+
+        // DELETE: api/Pessoa/5
+        public void Delete(int id)
+        {
+            Professores _pessoa = new Professores();
+            _pessoa.Deletar(id);
+
+
+        }
+    }
+}
---
+++ WebApi_Professores/WebApi_Professores/Model/Professor.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_Professores.Model
+{
+    public class Professores
+    {
+        public int Mid { get; set; }
+
+        public string Mnome { get; set; }
+
+        public string Mcredencial { get; set; }
+
+        public string Mdisciplina { get; set; }
+
+        public string Mhorario { get; set; }
+
+
+        public List<Professores> ListaProfessores()
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data\Base.json");
+            var json = File.ReadAllText(caminhoArquivo);
+            var ListaProfessores = JsonConvert.DeserializeObject<List<Professores>>(json);
+
+            return ListaProfessores;
+        }
+        public bool ReescreverArquivo(List<Professores> listaProfessores)
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data\base.json");
+            var json = JsonConvert.SerializeObject(listaProfessores, Formatting.Indented);
+            File.WriteAllText(caminhoArquivo, json);
+            return true;
+        }
+
+        public Professores Inserir(Professores Professor)
+        {
+            var ListaProfessores = this.ListaProfessores();
+            var maxId = ListaProfessores.Max(professor => professor.Mid);
+            Professor.Mid = maxId + 1;
+            ListaProfessores.Add(Professor);
+            ReescreverArquivo(ListaProfessores);
+            return Professor;
+        }
+
+        public Professores Atualizar(int Mid, Professores Professor)
+        {
+            var ListaProfessores = this.ListaProfessores();
+            var itemIndex = ListaProfessores.FindIndex(p => p.Mid == Mid);
+            if (itemIndex >= 0)
+            {
+                Professor.Mid = Mid;
+                ListaProfessores[itemIndex] = Professor;
+
+            }
+            else
+            {
+                return null;
+            }
+            ReescreverArquivo(ListaProfessores);
+            return Professor;
+        }
+
+        public bool Deletar(int id)
+        {
+            var ListaProfessores = this.ListaProfessores();
+            var itemIndex = ListaProfessores.FindIndex(p => p.Mid == id);
+            if (itemIndex >= 0)
+            {
+                ListaProfessores.RemoveAt(itemIndex);
+
+            }
+            else
+            {
+                return false;
+            }
+            ReescreverArquivo(ListaProfessores);
+            return true;
+        }
+    }
+}
